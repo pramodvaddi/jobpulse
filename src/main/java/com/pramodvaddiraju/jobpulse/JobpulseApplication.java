c4ba33e8be2ad1391e702c5af9b3940c6e653f26
Pramod Vaddiraju <pramodvaddiraju@gmail.com>
2025-06-07 14:57:34 +0530

    initial commit

---
+++ src/main/java/com/pramodvaddiraju/jobpulse/JobpulseApplication.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.jobpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobpulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pramodvaddiraju/jobpulse/JobpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pramodvaddiraju.jobpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
